02b4e97d12d4e6119e4c5dcb5a63ec27046f5628
ZAM ZAM <217864473+zamnice@users.noreply.github.com>
2025-08-25 20:54:33 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-  theme: { extend: {
-    colors: { merakitzam: { DEFAULT: "#FF6B00" } }
-  }},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        merakitzam: {
+          DEFAULT: "#FF6B00"
+        }
+      }
+    }
+  },
   plugins: [],
 }
